2fc0ef943cd9319731c57f54ef4968a084cde44e
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-18 16:02:49 +0200

    style

--- blackjack/app.js
+++ blackjack/app.js
@@ -1,10 +1,17 @@
 let card1 = 6
 let card2 = 9
 let sum = card1 + card2
+let hasBlackjack = false
+let isAlive = true
+let massage = ""
 
 if (sum <= 20) {
-    console.log("Do you want draw a new card? ")
-} else if (sum == 21)
-    console.log("Woohoo! you got a blackjack")
-else
-    console.log("Sorry you are out of game!")
+    massage = "Do you want draw a new card? "
+} else if (sum == 21) {
+    massage = "Woohoo! you got a blackjack"
+    hasBlackjack = true
+}
+else {
+    massage = "Sorry you are out of game!"
+    isAlive = false
+}
