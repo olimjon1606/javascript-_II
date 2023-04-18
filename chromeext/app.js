6a331755b4fe983880638bacbd5db7df22aab733
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-18 22:00:32 +0200

    json

--- chromeext/app.js
+++ chromeext/app.js
@@ -1,8 +1,25 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
-
+const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function () {
-    console.log("inputbtn clicked")
-
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItems = ""
+    for (i = 0; i < myLeads.length; i++) {
+        // listItems += "<li><a href='#'>"+myLeads[i]+"</a></li>"
+        listItems += `
+            <li>
+                <a href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+
+    }
+    ulEl.innerHTML = listItems
+}
