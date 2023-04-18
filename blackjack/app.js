04d543234dc02afe3283ca76afd5c13e636d1d87
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-18 15:52:24 +0200

    blackjack

---
+++ blackjack/app.js
@@ -0,0 +1,10 @@
+let card1 = 6
+let card2 = 9
+let sum = card1 + card2
+
+if (sum <= 20) {
+    console.log("Do you want draw a new card? ")
+} else if (sum == 21)
+    console.log("Woohoo! you got a blackjack")
+else
+    console.log("Sorry you are out of game!")
