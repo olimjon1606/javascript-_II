b1ddb7d9294017c626b4c7db975f0dd1c7ae04cf
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-19 09:03:39 +0200

    Update app.js

--- chromeext/app.js
+++ chromeext/app.js
@@ -1,43 +1,31 @@
 let myLeads = []
-// `["https://olimjonorifjonov.com/", "www.google.com"]`
-// myLeads = JSON.stringify(myLeads)
-// myLeads = JSON.parse(myLeads)
-// myLeads.push("www.youtube.com")
-// for (i = 0; i < 3; i++) {
-//     console.log(myLeads[i])
-// }
-// myLeads = JSON.stringify(myLeads)
-// console.log(typeof myLeads)
-
-
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-// localStorage.setItem("myLeads", "https://olimjonorifjonov.com/");
-// console.log(localStorage.getItem("myLeads"))
-
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-
-    console.log(localStorage.getItem("myLeads"))
+    render(myLeads)
 })
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage) 
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (i = 0; i < myLeads.length; i++) {
+    for (i = 0; i < leads.length; i++) {
         // listItems += "<li><a href='#'>"+myLeads[i]+"</a></li>"
         listItems += `
             <li>
-                <a href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -45,3 +33,9 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    render()
+})
