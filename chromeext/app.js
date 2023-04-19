764d7a6c501855698103cfa1489f9e4cac4a9411
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-19 08:38:40 +0200

    Update app.js

--- chromeext/app.js
+++ chromeext/app.js
@@ -1,12 +1,34 @@
 let myLeads = []
+// `["https://olimjonorifjonov.com/", "www.google.com"]`
+// myLeads = JSON.stringify(myLeads)
+// myLeads = JSON.parse(myLeads)
+// myLeads.push("www.youtube.com")
+// for (i = 0; i < 3; i++) {
+//     console.log(myLeads[i])
+// }
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
+
+
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
+
+// localStorage.setItem("myLeads", "https://olimjonorifjonov.com/");
+// console.log(localStorage.getItem("myLeads"))
+
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+
+    console.log(localStorage.getItem("myLeads"))
 })
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage) 
 
 function renderLeads() {
     let listItems = ""
